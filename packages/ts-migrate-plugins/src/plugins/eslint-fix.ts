4995998e9a90b7a80662c92a7cce522ec2e826b0
Pedro Paulo Bazzo Neto <developerpedropbazzo@gmail.com>
2020-09-12 11:13:31 -0300

    first commit

---
+++ packages/ts-migrate-plugins/src/plugins/eslint-fix.ts
@@ -0,0 +1,35 @@
+import { ESLint } from 'eslint';
+import { Plugin } from 'ts-migrate-server';
+
+const cli = new ESLint({
+  fix: true,
+  useEslintrc: true,
+  // Set ignore to false so we can lint in `tmp` for testing
+  ignore: false,
+});
+
+const eslintFixPlugin: Plugin = {
+  name: 'eslint-fix',
+  async run({ fileName, text }) {
+    try {
+      let newText = text;
+      while (true) {
+        // eslint-disable-next-line no-await-in-loop
+        const [report] = await cli.lintText(newText, {
+          filePath: fileName,
+        });
+
+        if (!report || !report.output || report.output === newText) {
+          break;
+        }
+        newText = report.output;
+      }
+      return newText;
+    } catch (e) {
+      console.error('Error occurred in eslint-fix plugin :(');
+      return text;
+    }
+  },
+};
+
+export default eslintFixPlugin;
